7ae5d7076e41877c8a71865ea321d76f142b72b1
dmitriew <hjq3301@mail.ru>
2020-10-12 12:12:55 +0700

    feat:add solution for v-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+  }
+
+  encrypt(message, key) {
+    const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const array = [];
+    let keyStage = -1;
+    for (let i = 0; i < message.length; i++) {
+      if (abc.indexOf(message[i].toUpperCase()) > -1) {
+        keyStage++;
+        let number = (abc.indexOf(message[i].toUpperCase()) + abc.indexOf(key[keyStage % key.length].toUpperCase())) % abc.length;
+        array.push(abc[number]);
+      } else {
+        array.push(message[i])
+      }
+    }
+    return this.direct ? array.join('') : array.reverse().join('');
+  }
+  decrypt(encryptedMessage, key) {
+    const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const array = [];
+    let keyStage = -1;
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (abc.indexOf(encryptedMessage[i].toUpperCase()) > -1) {
+        keyStage++;
+        let number = (abc.indexOf(encryptedMessage[i].toUpperCase()) + abc.length - abc.indexOf(key[keyStage % key.length].toUpperCase())) % abc.length;
+        array.push(abc[number])
+      } else {
+        array.push(encryptedMessage[i])
+      }
+    }
+    return this.direct ? array.join('') : array.reverse().join('');
   }
 }
+
 
 module.exports = VigenereCipheringMachine;
